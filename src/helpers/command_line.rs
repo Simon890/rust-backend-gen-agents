9387a7372ea51d9b048755dc137d6e98b98d773e
Simon890 <revellosimon890@gmail.com>
2025-05-04 16:00:29 -0300

    Add function to print out agent activity

--- src/helpers/command_line.rs
+++ src/helpers/command_line.rs
@@ -5,6 +5,33 @@ use crossterm::{
     ExecutableCommand
 };
 
+#[derive(PartialEq, Debug)]
+pub enum PrintCommand {
+    AICall,
+    UnitTest,
+    Issue
+}
+
+impl PrintCommand {
+    pub fn print_agent_message(&self, agent_pos: &str, agent_statement: &str) {
+        let mut stdout: std::io::Stdout = stdout();
+
+        let statement_color: Color = match self {
+            Self::AICall => Color::Cyan,
+            Self::UnitTest => Color::Magenta,
+            Self::Issue => Color::Red
+        };
+
+        stdout.execute(SetForegroundColor(Color::Green)).unwrap();
+        print!("[Agent] {}: ", agent_pos);
+        
+        stdout.execute(SetForegroundColor(statement_color)).unwrap();
+        println!("{}", agent_statement);
+
+        stdout.execute(ResetColor).unwrap();
+    }
+}
+
 /// Get user request
 pub fn get_user_response(question: &str) -> String {
     let mut stdout: std::io::Stdout = stdout();
@@ -21,4 +48,19 @@ pub fn get_user_response(question: &str) -> String {
         .expect("Failed to read response");
 
     user_response.trim().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_prints_agent_messages() {
+        PrintCommand::AICall
+            .print_agent_message("Managing Agent", "Testing testing, processing something...");
+        
+        PrintCommand::Issue
+            .print_agent_message("Managing Agent", "Found a bug on line 15");
+        
+    }
 }
